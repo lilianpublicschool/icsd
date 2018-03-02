3f2b9b6d07dc76a9f01812e68ec1ffb88929f91b
Azeem <azeemm@qualcomm.com>
2018-03-02 12:04:19 -0800

    Update Menus text and styling

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(function () {
     'use strict'
-    var GOOGLE_CALENDAR_ID = 'sisni95sgv8ifcq5su0d8j4khs%40group.calendar.google.com';
+    // var GOOGLE_CALENDAR_ID = 'sisni95sgv8ifcq5su0d8j4khs%40group.calendar.google.com';
+    var GOOGLE_CALENDAR_ID = 'km189pj4a5bptkna3f5gta4rgk@group.calendar.google.com';
     var GOOGLE_API_KEY = 'AIzaSyDzV4tcu-80oWSp0MsR5r2CFD1i6PgSTys';
     var calendarEventTemplateHtml = $('#calender-event-template').html();
     var calendarEventHtml;
@@ -10,7 +11,7 @@ $(function () {
         // url: "https://www.googleapis.com/calendar/v3/calendars/sisni95sgv8ifcq5su0d8j4khs%40group.calendar.google.com/events?maxResults=25&orderBy=startTime&singleEvents=true&timeMin=2018-02-16T10%3A00%3A00-07%3A00&fields=description%2Citems(colorId%2Ccreator(displayName%2Cself)%2Cdescription%2Cend%2FdateTime%2CendTimeUnspecified%2Cetag%2Cid%2Clocation%2Corganizer%2FdisplayName%2Crecurrence%2Csource%2Cstart%2FdateTime%2Cstatus%2Csummary)%2Csummary&key=AIzaSyDzV4tcu-80oWSp0MsR5r2CFD1i6PgSTys",
         url: "https://www.googleapis.com/calendar/v3/calendars/" + GOOGLE_CALENDAR_ID + "/events",
         data: {
-            maxResults: 7,
+            maxResults: 5,
             orderBy: 'startTime',
             singleEvents: true,
             timeMin: new Date().toISOString(),
@@ -23,10 +24,10 @@ $(function () {
             for (var i = 0; i < dataArray.length; i++) {
                 if(dataArray[i]["start"]["dateTime"]){
                     eventStartDateTime = moment(dataArray[i]["start"]["dateTime"]);
-                    if(dataArray[i]["end"]["date"]){
-                        eventTime = eventStartDateTime.format('hh:mm a') + " - " + moment(dataArray[i]["end"]["dateTime"]).format('hh:mm a');
+                    if(dataArray[i]["end"]["dateTime"]){
+                        eventTime = eventStartDateTime.format('h:mma') + " - " + moment(dataArray[i]["end"]["dateTime"]).format('h:mma');
                     } else {
-                        eventTime = eventStartDateTime.format('hh:mm a');
+                        eventTime = eventStartDateTime.format('h:mma');
                     }
                 } else {
                     eventStartDateTime = moment(dataArray[i]["start"]["date"]);
